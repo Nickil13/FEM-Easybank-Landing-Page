89647252b55925ae36b5fc3cd6c1e7fd91d12844
Nicki Lindstrom <lindstromn13@gmail.com>
2022-07-09 22:23:36 -0600

    Added tailwind theme specifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx}"],
     theme: {
-        extend: {},
+        colors: {
+            primary: {
+                "dark-blue": "hsl(233, 26%, 24%)",
+                "lime-green": "hsl(136, 65%, 51%)",
+                "bright-cyan": "hsl(192, 70%, 51%)",
+            },
+            neutral: {
+                "grayish-blue": "hsl(233, 8%, 62%)",
+                "light-grayish-blue": "hsl(220, 16%, 96%)",
+                "very-light-gray": "hsl(0, 0%, 98%)",
+                white: "hsl(0, 0%, 100%)",
+            },
+        },
+        extend: {
+            fontSize: {
+                body: "1.125rem",
+            },
+        },
     },
     plugins: [],
 };
